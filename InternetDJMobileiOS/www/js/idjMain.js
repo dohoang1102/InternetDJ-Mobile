0c5845ed545f42a3eebe03895c197bbcc24cf8b7
Michael Bordash <michael@xtify.com>
2012-06-04 13:46:45 -0400

    Added playlist functionality.  This update features database storage creation via phone gap's storage API, "Add to Playlist" button on the song view panel, persistence of the songId within the database, and an html5 audio playlist player that's populated from the local playlist database.

--- InternetDJMobileiOS/www/js/idjMain.js
+++ InternetDJMobileiOS/www/js/idjMain.js
@@ -5,10 +5,17 @@
 // http://www.internetdj.com
 
 $(document).bind("mobileinit", function(){
-  $.mobile.defaultPageTransition = 'none';
+
+    $.mobile.defaultPageTransition = 'none';
+
 });
 
+var db = '';
+
 $(document).ready(function(){
+
+    // click event handlers
+
     $(".pageNavPrev").click(function () {
         var genreName = $('#genreName').text();
         var genreId = $('#genreId').text();
@@ -26,10 +33,61 @@ $(document).ready(function(){
         
         loadGenreArtists(genreId,genreName,total,page);
     });
+    
+    $("#li-sotd").click(function() {
+        loadSOTD();
+        return false;
+    });
+    
+    $("#li-popsongs").click(function() {
+        loadPopSongList();
+        return false;
+    });
+    
+    $("#li-songGenres").click(function() {
+        loadGenreList('loadGenreSongs','#songGenreList');
+        return false;
+    });
+    
+    $("#li-artistGenres").click(function() {
+        loadGenreList('loadGenreArtists','#artistGenreList');
+        return false;
+    });
+    
+    $("#addToPlaylist").click(function() {
+    
+        db = window.openDatabase("internetdj", "1.0", "internetdj", 1000000);
+
+        var songId = $('#addToPlaylist').find('a:first').attr('id');
+        if(songId) {
+            db.transaction(populateDB, errorCB, successCB);
+        } else {
+            alert("Song for Playlist not Provided");
+        }
+        return false;
+    });
+
+    $("#li-showPlaylist").click(function() {
+        loadPlaylist();
+        return false;
+    });
 });
 
 
+function populateDB(tx) {
+    var songId = $('#addToPlaylist').find('a:first').attr('id');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS playlist (id unique, songId)');
+    tx.executeSql('INSERT INTO playlist (songId) VALUES ("' + songId + '")');
+}
 
+function errorCB(err) {
+        alert("Error processing SQL: " + jQuery.param(err));
+}
+
+function successCB() {
+        alert("Song added to your playlist!");
+}
+    
 function getXidFromNative() {
  
   callXidNativeFunction(
@@ -49,27 +107,15 @@ function callXidNativeFunction(types, success, fail) {
   return PhoneGap.exec(success, fail, "PushNotification", "printXid", types);
 }
 
-
 function loadSOTD() {
-    $('#sotdTitle').html('');
-    $('#sotdArtist').html('');
-    $('#sotdImage').html('');
-    $('#sotdPlay').html('');
 
     $.getJSON("http://www.internetdj.com/developers/api.php?api_key=1291928384728192&request_type=sotd&" + new Date().getTime(), function(data){
-            
-        var sotdTitle = "<span style='font-weight: bold'>Title:</span> " + data[0].song_title;
-        var sotdArtist = "<span style='font-weight: bold'>Artist:</span> <a id='" + data[0].artist_id + "' href='#' onClick='javascript:loadArtist(\"" + data[0].artist_id + "\",\"" + escape(data[0].artist_name) + "\");'>" + data[0].artist_name + "</a>";
-        var sotdId = data[0].song_id;
-        var sotdImage = data[0].song_image_url;
 
-        $('#sotdTitle').html(sotdTitle);
-        $('#sotdArtist').html(sotdArtist);
-    
-        $('#sotdPlay').html('<audio controls><source src="http://www.internetdj.com/artists.php?op=stream&song=' + sotdId + '" preload="auto" /></audio>');
-        $('#sotdImage').html('<img width="200" height="200" src="'+sotdImage+'" />');
+        var sotdId = data[0].song_id;
+        loadSong(sotdId);
     });
 }
+
 
 function loadPopSongList() {
     $('#popSongsList').html('');
@@ -89,10 +135,14 @@ function loadPopSongList() {
         });
         $('#popSongsList').append(songListItem);
         $('#popSongsList').listview('refresh');
+        
     });
+    $("#showPopSongs").click();
+
 }
 
 function loadGenreList(jsFunc,divUpdate) {
+   
     $.getJSON("http://www.internetdj.com/developers/api.php?api_key=1291928384728192&request_type=genre&" + new Date().getTime(), function(genres){
         $.each(genres, function(i,genre) {
             var s_gen = genre.genre_name;
@@ -113,6 +163,11 @@ function loadGenreList(jsFunc,divUpdate) {
         });
         $(divUpdate).listview('refresh');
     });
+    if(jsFunc == "loadGenreArtists") {
+        $("#showArtistGenres").click();
+    } else {
+        $("#showGenres").click();
+    }
 }
 
 function loadGenreSongs(genreId,genreName) {
@@ -224,7 +279,8 @@ function loadSong(songId) {
     $('#songArtist').html('');
     $('#songImage').html('');
     $('#songPlay').html('');
-                    
+    $('#addToPlaylist').html('');           
+
     $.getJSON("http://www.internetdj.com/developers/api.php?api_key=1291928384728192&request_type=song&song_id=" + songId + "&" + new Date().getTime(), function(data){
     
         var songTitle = "<span style='font-weight: bold'>Title:</span> " + data[0].song_title;
@@ -236,6 +292,103 @@ function loadSong(songId) {
         $('#songArtist').html(songArtist);
         $('#songPlay').html('<audio controls><source src="http://www.internetdj.com/artists.php?op=stream&song=' + songId + '" preload="auto" /></audio>');
         $('#songImage').html('<img width="200" height="200" src="'+songImage+'" />');
+        $('#addToPlaylist').html('<a id="' + songId + '" data-mini="true" href="#" data-role="button" data-icon="plus" data-iconpos="right" data-theme="d">Add to Playlist</a>');
+        $('#addToPlaylist').button(); 
+        
     });
-    $("#showSong").click();
+    $('#showSong').click();
 }
+
+function loadPlaylist() {                
+
+    // get playlist items from DB
+
+    db = window.openDatabase("internetdj", "1.0", "internetdj", 1000000);
+    
+    db.transaction(queryDBPlaylist, errorCB);
+
+    $('#clickPlaylist').click();
+}
+
+function queryDBPlaylist(tx) {
+    tx.executeSql('SELECT * FROM playlist', [], querySuccess, errorCB);
+}
+
+
+var next = 0;
+var urls = [];
+
+function querySuccess(tx, results) {
+
+    // for each song ID get meta data from internetdj api endpoint
+    var len = results.rows.length;
+    
+    var playlistItems = '';
+    $('#showPlaylistItems').html('');
+    
+    for (var i=0; i<len; i++) {
+        var songId = results.rows.item(i).songId;
+        urls[i] = 'http://www.internetdj.com/artists.php?op=stream&song=' + songId;
+
+        $.getJSON("http://www.internetdj.com/developers/api.php?api_key=1291928384728192&request_type=song&song_id=" + songId + "&" + new Date().getTime(), function(data) {
+            
+            var songTitle = data[0].song_title;
+            var songArtist = data[0].artist_name;
+            if(songTitle) {
+                playlistItems += '<a href="#" onClick="pickSong(' + i + ')">' + songArtist + ' - ' + songTitle + '</a><br />';
+            
+                $('#showPlaylistItems').append(playlistItems);
+            }
+        });
+    }
+    loadPlayer();
+}
+
+function errorCB(err) {
+    alert("Error processing SQL: "+err.code);
+}
+
+
+function loadPlayer() {
+    var audioPlayer = new Audio();
+    audioPlayer.controls="controls";
+    audioPlayer.addEventListener('ended',nextSong,false);
+    audioPlayer.addEventListener('error',errorFallback,true);
+    document.getElementById("player").appendChild(audioPlayer);
+    nextSong();
+}
+function nextSong() {
+    if(urls[next]!=undefined) {
+        var audioPlayer = document.getElementsByTagName('audio')[0];
+        if(audioPlayer!=undefined) {
+            audioPlayer.src=urls[next];
+            audioPlayer.load();
+            audioPlayer.play();
+            next++;
+        } else {
+            loadPlayer();
+        }
+    } else {
+        //alert('the end!');
+    }
+}
+function errorFallback() {
+        nextSong();
+}
+function playPause() {
+    var audioPlayer = document.getElementsByTagName('audio')[0];
+    if(audioPlayer!=undefined) {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+        } else {
+            audioPlayer.pause();
+        }
+    } else {
+        loadPlayer();
+    }
+}
+function pickSong(num) {
+    next = num;
+    nextSong();
+}
+
